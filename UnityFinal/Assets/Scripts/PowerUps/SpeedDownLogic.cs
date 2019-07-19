e4c984f1d86b3b7664cf4a33e8807164ad5b6e36
MmmmHeee <43287140+MmmmHeee@users.noreply.github.com>
2019-07-19 18:52:03 +0800

    新增陷阱与陷阱随机生成逻辑

---
+++ UnityFinal/Assets/Scripts/PowerUps/SpeedDownLogic.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class SpeedDownLogic : MonoBehaviour
+{
+    const float EffectTime = 5.0f;
+    float m_effectTimer;
+    bool isActivated;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GetComponent<PowerUpLogic>().OnCastStart += Cast;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        m_effectTimer += Time.deltaTime;
+        if (m_effectTimer > EffectTime) {
+            ResetEffect();
+            Destroy(gameObject);
+        }     
+    }
+
+    void Cast() {          
+        m_effectTimer = 0;
+        ApplySpeedDownEffect();        
+    }
+
+    void ApplySpeedDownEffect() {
+        transform.parent.GetComponent<PlayerController>().applySpeedDown();
+    }
+
+    void ResetEffect() {
+        transform.parent.GetComponent<PlayerController>().resetSpeed();
+    }
+
+    private void OnDestroy() {
+        PlayerController player = transform.parent.GetComponent<PlayerController>();
+        if (player) {
+            player.resetSpeed();
+        }    
+    }
+}
---
+++ UnityFinal/Assets/Scripts/PowerUps/SpeedUpLogic.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class SpeedUpLogic : MonoBehaviour
+{
+    const float EffectTime = 5.0f;
+    float m_effectTimer;
+    bool isActivated;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GetComponent<PowerUpLogic>().OnCastStart += Cast;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        m_effectTimer += Time.deltaTime;
+        if (m_effectTimer > EffectTime) {
+            ResetEffect();
+            Destroy(gameObject);
+        }     
+    }
+
+    void Cast() {          
+        m_effectTimer = 0;
+        ApplySpeedUpEffect();        
+    }
+
+
+    void ApplySpeedUpEffect() {
+        transform.parent.GetComponent<PlayerController>().applySpeedUp();
+    }
+
+    void ResetEffect() {
+        transform.parent.GetComponent<PlayerController>().resetSpeed();
+    }
+
+    private void OnDestroy() {
+        PlayerController player = transform.parent.GetComponent<PlayerController>();
+        if (player) {
+            player.resetSpeed();
+        }    
+    }
+}
