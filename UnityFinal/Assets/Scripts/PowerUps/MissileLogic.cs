2b19521b8a5ec8cd938f3e89b7709c9e1519357f
hugo7650 <31364658+hugo7650@users.noreply.github.com>
2019-07-20 07:05:21 +0800

    导弹半完成

---
+++ UnityFinal/Assets/Scripts/PowerUps/MissileLogic.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MissileLogic : MonoBehaviour
+{
+    const float EffectTime = 15.0f;
+    float m_effectTimer;
+
+    GameObject m_parent;
+
+    GameObject m_target = null;
+
+    NavMeshAgent m_navMeshAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_effectTimer = 0;
+
+        m_navMeshAgent = GetComponent<NavMeshAgent>();
+        m_navMeshAgent.enabled = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {        
+        m_effectTimer += Time.deltaTime;
+        if (m_effectTimer > EffectTime) {            
+            Destroy(gameObject);
+        }
+        if(m_target)
+        {
+            Debug.Log(m_navMeshAgent.enabled);
+            m_navMeshAgent.enabled = true;
+            m_navMeshAgent.SetDestination(m_target.transform.position);
+        }
+        else
+            m_navMeshAgent.enabled = false;
+
+        if (GetComponentInChildren<DamageSourceLogic>() == null) {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetTarget(GameObject target)
+    {
+        m_target = target;
+        
+        NavMeshHit closestHit;
+        if( NavMesh.SamplePosition(  transform.position, out closestHit, 500, 1 ) ){
+            transform.position = closestHit.position;
+            gameObject.AddComponent<NavMeshAgent>();
+        }
+        
+    }
+}
