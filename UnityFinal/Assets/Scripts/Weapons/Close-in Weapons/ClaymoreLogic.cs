bacb4b2c7e3cfb598195206963d6c87ab4b3c57d
Zhouyang Fang <fangzhy1998@gmail.com>
2019-07-19 14:31:42 +0800

    添加武器动画挥砍中心点
    Temp2
    
    
    Add Weapon Center

---
+++ UnityFinal/Assets/Scripts/Weapons/Close-in Weapons/ClaymoreLogic.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Special Weapon Script For dagger
+public class ClaymoreLogic : MonoBehaviour
+{
+    Animator animator;
+    [SerializeField]
+    int Damage = 1;
+    [SerializeField]
+    DamageSourceLogic m_damageSource = null;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        GetComponent<WeaponLogic>().OnAttackStart += Attack; // When OnAttackStart is called, Attack is called
+        m_damageSource.gameObject.SetActive(false); // Deactivate the DamageSource at default
+    }
+
+    void Attack() {
+        animator.SetTrigger("Attack");
+    }
+
+    void activateDamageSource() {
+        // Animation Events
+        
+        m_damageSource.gameObject.SetActive(true);
+        m_damageSource.InitDamageSourceInfo(GetComponentInParent<PlayerLogic>().getPlayerID(), Damage); 
+        // Initialize the damage information after the DamageSource is activated
+    }
+
+    void deactivateDamageSource() {
+        // Animation Events
+        
+        m_damageSource.gameObject.SetActive(false);             
+    }
+
+
+}
